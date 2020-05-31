e6b7f24cb0dfd908b33c2df771e7ce4781222be6
Sam van Kampen <sam@tehsvk.net>
2020-05-31 17:28:54 +0200

    Update figure, reorder some text, add explanation of periodic task sets

--- plot_ub_rm.py
+++ plot_ub_rm.py
@@ -1,6 +1,12 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
+
+fig = plt.gcf()
+fig.set_size_inches(6,3.5)
+
 plt.xlabel("$T_2$ / $T_1$")
 plt.ylabel(r"$U_{ub}$")
 plt.ylim(0.5, 1)
@@ -12,6 +18,7 @@ def U(F, k):
 k = np.linspace(1, 10, 1000)
 F = np.floor(k)
 Us = U(F, k)
+
 
 plt.plot(k, Us)
 plt.savefig("ub_rm_2tasks.eps")
